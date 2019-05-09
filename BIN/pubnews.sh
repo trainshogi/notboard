1197191a53eb260b5802ff2360886c8b970ccfa9
gidoichi <shinichi@FJITSU-ESPRIMO.localdomain>
2019-05-09 10:12:19 +0900

    バグ修正およびオプション追加

--- BIN/pubnews.sh
+++ BIN/pubnews.sh
@@ -4,7 +4,7 @@
 #
 # PUBNEWS.SH : 新着情報の連絡
 #
-# Written by Shinichi Yanagido (s.yanagido@gmail.com) on 2019-05-05
+# Written by Shinichi Yanagido (s.yanagido@gmail.com) on 2019-05-09
 #
 ######################################################################
 
@@ -24,8 +24,11 @@ export UNIX_STD=2003  # to make HP-UX conform to POSIX
 # === Define the functions for printing usage and exiting ============
 print_usage_and_exit () {
   cat <<-USAGE 1>&2
-	Usage   : ${0##*/}
-	Version : 2019-05-05 23:05:02 JST
+	Usage   : ${0##*/} [options]
+	Options : -n|--dry-run
+	          -s|--not-update
+	          -o|--to-stdout
+	Version : 2019-05-09 10:11:23 JST
 	USAGE
   exit 1
 }
@@ -56,11 +59,35 @@ case "$# ${1:-}" in
 esac
 
 # === Initialize parameters ==========================================
+dryrun=0
+noupdate=0
+tostdout=0
 date=''
 title=''
 category=''
 from=''
 ref=''
+
+# === Read options ===================================================
+while :; do
+  case "${1:-}" in
+    --dry-run|-n)    dryrun=1
+                     shift
+                     ;;
+    --not-update|-s) noupdate=1
+                     shift 1
+                     ;;
+    --to-stdout|-o)  tostdout=1
+                     shift 1
+                     ;;
+    --|-)            break
+                     ;;
+    --*|-*)          error_exit 1 'Invalid option'
+                     ;;
+    *)               break
+                     ;;
+  esac
+done
 
 
 ######################################################################
@@ -79,111 +106,75 @@ used_twitter_id=$(grep MY_scname                                       \
                   sed "s/'//g"                                         )
 [ "$used_twitter_id" != "$twitter_id" ] && error_exit 1 '配信元情報の整合性がありません'
 # --- 1.フォロワの取得および更新
-update-subscriber.sh -f "$Dir_tmp/subscriber"
+[ $noupdate -eq 0 ] && update-subscriber.sh -f "$Dir_tmp/subscriber"
 
 # === 新着情報を取得して配信 =========================================
-# --- a.学科掲示板からの取得，配信
 key=''
 delimiter=''
-getcsnews.sh -f "$Dir_tmp/boardcs_latest"                 |
-# 1:group 2:key 3:value                                   #
-while IFS= read -r line; do                               #
-  if [ "$key" != "${line%% *}" ]; then                    #
-    key="${line%% *}"                                     #
-    date=''                                               #
-    title=''                                              #
-    category=''                                           #
-    from=''                                               #
-    ref=''                                                #
-    printf "$delimiter"                                   #
-    delimiter='\0'                                        #
-  fi                                                      #
-  case "${line#* }" in                                    #
-    date*)     echo '【日付】'                            #
-               printf "%s\n" "$line"                      |
-               cut -d ' ' -f 3-                           |
-               sed 's/\\n/\n/g'                           #
-               ;;                                         #
-    title*)    echo '【タイトル】'                        #
-               printf "%s\n" "$line"                      |
-               cut -d ' ' -f 3-                           |
-               sed 's/\\n/\n/g'                           #
-               ;;                                         #
-    category*) echo '【カテゴリ】'                        #
-               printf "%s\n" "$line"                      |
-               cut -d ' ' -f 3-                           |
-               sed 's/\\n/\n/g'                           #
-               ;;                                         #
-    from*)     echo '【担当者】'                          #
-               printf "%s\n" "$line"                      |
-               cut -d ' ' -f 3-                           |
-               sed 's/\\n/\n/g'                           #
-               ;;                                         #
-    ref*)      echo '【詳細】'                            #
-               printf "%s\n" "$line"                      |
-               cut -d ' ' -f 3-                           |
-               sed 's/\\n/\n/g'                           #
-               ;;                                         #
-    *)         echo "【$(echo $line | cut -d ' ' -f 2)】" #
-               printf "%s\n" "$line"                      |
-               cut -d ' ' -f 3-                           |
-               sed 's/\\n/\n/g'                           #
-  esac                                                    #
-done                                                      |
-xargs -0 -I @ sh -c 'cat "'"$Dir_tmp"'/subscriber"  |     #
-                     cut -d " " -f 1                |     #
-                     xargs -I % dmtweet.sh -t % "@"'
-# --- b.学校掲示板からの取得，配信
-key=''
-delimiter=''
-gettuatnews.sh -f "$Dir_tmp/boardtuat_latest"             |
-# 1:group 2:key 3:value                                   #
-while IFS= read -r line; do                               #
-  if [ "$key" != "${line%% *}" ]; then                    #
-    key="${line%% *}"                                     #
-    date=''                                               #
-    title=''                                              #
-    category=''                                           #
-    from=''                                               #
-    ref=''                                                #
-    printf "$delimiter"                                   #
-    delimiter='\0'                                        #
-  fi                                                      #
-  case "${line#* }" in                                    #
-    date*)     echo '【日付】'                            #
-               printf "%s\n" "$line"                      |
-               cut -d ' ' -f 3-                           |
-               sed 's/\\n/\n/g'                           #
-               ;;                                         #
-    title*)    echo '【タイトル】'                        #
-               printf "%s\n" "$line"                      |
-               cut -d ' ' -f 3-                           |
-               sed 's/\\n/\n/g'                           #
-               ;;                                         #
-    category*) echo '【カテゴリ】'                        #
-               printf "%s\n" "$line"                      |
-               cut -d ' ' -f 3-                           |
-               sed 's/\\n/\n/g'                           #
-               ;;                                         #
-    from*)     echo '【担当者】'                          #
-               printf "%s\n" "$line"                      |
-               cut -d ' ' -f 3-                           |
-               sed 's/\\n/\n/g'                           #
-               ;;                                         #
-    ref*)      echo '【詳細】'                            #
-               printf "%s\n" "$line"                      |
-               cut -d ' ' -f 3-                           |
-               sed 's/\\n/\n/g'                           #
-               ;;                                         #
-    *)         echo "【$(echo $line | cut -d ' ' -f 2)】" #
-               printf "%s\n" "$line"                      |
-               cut -d ' ' -f 3-                           |
-               sed 's/\\n/\n/g'                           #
-  esac                                                    #
-done                                                      |
-xargs -0 -I @ sh -c 'cat "'"$Dir_tmp"'/subscriber"  |     #
-                     cut -d " " -f 1                |     #
-                     xargs -I % dmtweet.sh -t % "@"'
+if [ $dryrun -eq 0 ]; then                            #
+  getcsnews.sh   -f "$Dir_tmp/boardcs_latest"         #
+  gettuatnews.sh -f "$Dir_tmp/boardtuat_latest"       #
+else                                                  #
+  getcsnews.sh   -n -f "$Dir_tmp/boardcs_latest"      #
+  gettuatnews.sh -n -f "$Dir_tmp/boardtuat_latest"    #
+fi                                                    |
+# 1:group 2:key 3:value                               #
+while IFS= read -r line; do                           #
+  if [ "$key" != "${line%% *}" ]; then                #
+    key="${line%% *}"                                 #
+    date=''                                           #
+    title=''                                          #
+    category=''                                       #
+    from=''                                           #
+    ref=''                                            #
+    printf "$delimiter"                               #
+    delimiter='\n'                                    #
+  fi                                                  #
+  case "${line#* }" in                                #
+    date*)     printf '%s' "【日付】\n"               #
+               printf '%s' "$line\n"                  |
+               cut -d ' ' -f 3-                       |
+               sed -z 's/\n//'                        #
+               ;;                                     #
+    title*)    printf '%s' '【タイトル】\n'           #
+               printf '%s' "$line\n"                  |
+               cut -d ' ' -f 3-                       |
+               sed -z 's/\n//'                        #
+               ;;                                     #
+    category*) printf '%s' '【カテゴリ】\n'           #
+               printf '%s' "$line\n"                  |
+               cut -d ' ' -f 3-                       |
+               sed -z 's/\n//'                        #
+               ;;                                     #
+    from*)     printf '%s' '【担当者】\n'             #
+               printf '%s' "$line\n"                  |
+               cut -d ' ' -f 3-                       |
+               sed -z 's/\n//'                        #
+               ;;                                     #
+    ref*)      printf '%s' '【詳細】\n'               #
+               printf '%s' "$line\n"                  |
+               cut -d ' ' -f 3-                       |
+               sed -z 's/\n//'                        #
+               ;;                                     #
+    *)         printf '%s '"【$(echo $line            |
+                                cut -d ' ' -f 2)】\n" #
+               printf '%s' "$line\n"                  |
+               cut -d ' ' -f 3-                       |
+               sed -z 's/\n//'                        #
+               ;;                                     #
+  esac                                                #
+done                                                  |
+sed 's/\\n$//'                                        |
+sed -z 's/$/\n/'                                      |
+while IFS= read -r line; do                           #
+  [ $tostdout -eq 1 ] && echo "$line\n"               #
+  [ $dryrun -eq 1 ] && continue                       #
+  cat "$Dir_tmp/subscriber"                           |
+  cut -d ' ' -f 1                                     |
+  xargs -I @ $([ $nopublish -eq 1 ] && echo 'echo')   \
+             sh -c 'echo "'"$line"'"                  |
+                    dmtweet.sh -t @'                  #
+done
 
 
 ######################################################################
